0b55587c699218e5ad0a83757caa2809f5858c21
LuisAntoniokh <98839670+LuisAntoniokh@users.noreply.github.com>
2023-02-17 17:12:35 -0600

    Create receta.py

---
+++ receta.py
@@ -0,0 +1,11 @@
+class receta:
+  def _init_(self, paciente, fecha_nac, doctor, colegiado,
+               fecha_cita, hora_cita, tipo_consulta, tratamiento):
+    self.paciente = paciente
+    self.fecha_nac = fecha_nac
+    self.doctor = doctor
+    self.colegiado = colegiado
+    self.fecha_cita = fecha_cita
+    self.hora_cita = hora_cita
+    self.tipo_consulta = tipo_consulta
+    self.tratamiento = tratamiento
